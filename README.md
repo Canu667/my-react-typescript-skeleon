31791c8ae16613729c2c7ef8f25d36645fe806ab
Mateusz Canova <mcanu667@gmail.com>
2019-08-26 07:03:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-react-typescript-skeleton
+
+Some boilerplate code I will use for React projects with Typescript
---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/index.css";
+
+class App extends React.Component<IAppProps, IAppState> {
+  public render() {
+    return (
+      <div>
+        <h1>Witaj Świecie!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
